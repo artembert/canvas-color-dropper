e4dd3762d2ce0574b8500d342293aab77f9f5f26
Artem Beresnev <artembert@gmail.com>
2023-08-09 00:49:50 +0400

    adjust styles for toolbar

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -1,3 +1,4 @@
+import pickerIcon from '../../assets/color-picker.svg'
 import styles from './styles.module.css';
 
 type Props = {
@@ -11,7 +12,10 @@ export const Toolbar = (props: Props) => {
 
     return (
         <div className={styles.toolbar}>
-            <button type='button' onClick={onTogglePicker}>[Picker]</button>
+            <button className={`${styles.colorPickerButton} ${isPickerSelected ? styles.colorPickerButtonActive : ''}`}
+                    type='button' onClick={onTogglePicker}>
+                <img src={pickerIcon} alt={'Color Picker icon'}/>
+            </button>
             {isPickerSelected &&
                 <div className={styles.currentColor}>{currentColor}</div>}
         </div>
